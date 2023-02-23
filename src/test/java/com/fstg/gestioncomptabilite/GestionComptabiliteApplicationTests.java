946ab348fa6cfd90efac610e7809b6a60c6c2f59
ENNASTY <ennastyhamza42@gmail.com>
2023-02-23 13:10:40 +0100

    first commit

---
+++ src/test/java/com/fstg/gestioncomptabilite/GestionComptabiliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstg.gestioncomptabilite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionComptabiliteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
