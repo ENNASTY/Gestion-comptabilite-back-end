d9d3bdc552a7fb66db37c53efa20440cafe723ca
ENNASTY <ennastyhamza42@gmail.com>
2023-02-27 23:07:18 +0100

    beans added

--- src/main/java/com/fstg/gestioncomptabilite/bean/DeclarationIREmployee.java
+++ src/main/java/com/fstg/gestioncomptabilite/bean/DeclarationIREmployee.java
@@ -21,7 +21,6 @@ public class DeclarationIREmployee {
     @ManyToOne
     private TauxIR tauxIR;
 
-<<<<<<< HEAD
     public TauxIR getTauxIR() {
         return tauxIR;
     }
@@ -29,8 +28,6 @@ public class DeclarationIREmployee {
     public void setTauxIR(TauxIR tauxIR) {
         this.tauxIR = tauxIR;
     }
-=======
->>>>>>> 7411f171fc0a261d144a1ba66f7bfac23aed70cd
 
     public void setId(Long id) {
         this.id = id;
